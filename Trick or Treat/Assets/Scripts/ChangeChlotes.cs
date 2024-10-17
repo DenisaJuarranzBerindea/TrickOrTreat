ddacc4891142de7e88289f5f170a5de32331fd6e
Paulalemany <palemany@ucm.es>
2024-10-17 17:29:28 +0200

    Se pueden quitar y poner cosas

---
+++ Trick or Treat/Assets/Scripts/ChangeChlotes.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeChlotes : MonoBehaviour
+{
+
+    #region References
+
+    #endregion
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
