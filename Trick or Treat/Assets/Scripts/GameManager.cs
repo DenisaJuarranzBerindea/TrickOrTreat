3a3171599f807f60751468ff7c797a61dc46677d
(i)nés <ineprimo@ucm.es>
2024-10-19 22:29:03 +0200

    HAY PUNTUACION?¿?¿?¿?

--- Trick or Treat/Assets/Scripts/GameManager.cs
+++ Trick or Treat/Assets/Scripts/GameManager.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
+
+
+public enum RESULT { PLAYER1, PLAYER2, TIE };
 
 public class GameManager : MonoBehaviour
 {
@@ -80,6 +84,22 @@ public class GameManager : MonoBehaviour
         {
             changeState(GameStates.START);
         }
+        if (Input.GetKeyUp(KeyCode.F))          // PRUEBA PARA PUNTUAR
+        {
+            RESULT res = judgeCostumes();
+
+            if (res == RESULT.PLAYER1)
+            {
+                Debug.Log("PLAYER 1");
+            }   
+            else if(res == RESULT.PLAYER2)
+            {
+                Debug.Log("PLAYER 2");
+            }
+            else { 
+                Debug.Log("TIE");
+            }
+        }
     }
 
     public GameStates getCurrentState() { return _currentState; }
@@ -116,27 +136,21 @@ public class GameManager : MonoBehaviour
 
 
 
-
-
-
-
-
-
-
-
     #region PUNTOS
 
 
     public void setNeighbour(GameObject n) { _currentNeighbour = n; }
 
-    public void judgeCostumes()
+    public RESULT judgeCostumes()
     {
         // puntuaciones de los jugadores
         Puntuacion p1 = _currentNeighbour.GetComponent<NeighbourScript>().judgeCostume(_player1);
+
+
         Puntuacion p2 = _currentNeighbour.GetComponent<NeighbourScript>().judgeCostume(_player2);
 
         //
-        bool resultados = decideVerdict(p1, p2);
+        return decideVerdict(p1, p2);
     }
 
     /// <summary>
@@ -145,30 +159,30 @@ public class GameManager : MonoBehaviour
     /// <param name="p1"></param>
     /// <param name="p2"></param>
     /// <returns></returns>
-    bool decideVerdict(Puntuacion p1, Puntuacion p2)
+    RESULT decideVerdict(Puntuacion p1, Puntuacion p2)
     {
         int puntos1 = 0, puntos2 = 0;
 
         Neighbour n = _currentNeighbour.GetComponent<NeighbourScript>().GetNeighbour();
 
+        // la diferencia minima, sin contar los iguales
         if (Mathf.Abs(p1.cute - n.cute) > Mathf.Abs(p2.cute - n.cute)) puntos1++;
-        else puntos2++;
+        else if((Mathf.Abs(p1.cute - n.cute) < Mathf.Abs(p2.cute - n.cute))) puntos2++;
         if (Mathf.Abs(p1.spooky - n.spooky) > Mathf.Abs(p2.spooky - n.spooky)) puntos1++;
-        else puntos2++; 
-        if (Mathf.Abs(p1.cute - n.cute) > Mathf.Abs(p2.cute - n.funny)) puntos1++;
-        else puntos2++;
+        else if (Mathf.Abs(p1.spooky - n.spooky) > Mathf.Abs(p2.spooky - n.spooky)) puntos2++; 
+        if (Mathf.Abs(p1.funny - n.funny) > Mathf.Abs(p2.funny - n.funny)) puntos1++;
+        else if (Mathf.Abs(p1.funny - n.funny) > Mathf.Abs(p2.funny - n.funny)) puntos2++;
+
+        puntos2 += p2.theme;
+        puntos1 += p1.theme;
 
 
 
         if (puntos1 > puntos2)
-        {
-            return true;
-        }
-    
-        else { 
-            return false;
-        
-        }
+            return RESULT.PLAYER1;
+        else if (puntos1 < puntos2)
+            return RESULT.PLAYER2;
+        else return RESULT.TIE;
     }
 
 
--- Trick or Treat/Assets/Scripts/Points/NeighbourScript.cs
+++ Trick or Treat/Assets/Scripts/Points/NeighbourScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,24 +24,50 @@ public class NeighbourScript : MonoBehaviour
 
    public Puntuacion judgeCostume(GameObject p)
     {
+
         Puntuacion aux;
+        aux = new Puntuacion();
         int themeCount = 0;
 
         OutfitScript outfit = p.GetComponent<OutfitScript>();
 
 
-        // puntuacion basica
-        aux.cute = (outfit.getTop().cute + outfit.getBottom().cute + outfit.getExtra().cute)/3;
-        aux.spooky = (outfit.getTop().spooky + outfit.getBottom().spooky + outfit.getExtra().spooky) /3;
-        aux.funny = (outfit.getTop().funny + outfit.getBottom().funny + outfit.getExtra().funny)/3;
+        Garment top = outfit.getTop();
+        Garment bot = outfit.getBottom();
+        Garment ex = outfit.getExtra();
+
+        if (top != null)
+        {
+
+            aux.cute += top.cute;
+            aux.spooky += top.spooky;
+            aux.funny += top.funny;
+
+        }
+        if (bot != null)
+        {
+            aux.cute += bot.cute;
+            aux.spooky += bot.spooky;
+            aux.funny += bot.funny;
+        }
+        if (ex != null)
+        {
+            aux.cute += ex.cute;
+            aux.spooky += ex.spooky;
+            aux.funny += ex.funny;
+        }
+
+        aux.cute = aux.cute/3;
+        aux.spooky = aux.spooky/3;
+        aux.funny = aux.funny/3;
 
 
         // SERIE
-        if(neighbour.series != SERIES.NONE)
+        if (neighbour.series != SERIES.NONE)
         {
-            if (outfit.getTop().series == neighbour.series) { themeCount++; }
-            if (outfit.getBottom().series == neighbour.series) themeCount++;
-            if (outfit.getExtra().series == neighbour.series) themeCount++;
+            if (top != null && top.series == neighbour.series) { themeCount++; }
+            if (bot != null && bot.series == neighbour.series) themeCount++;
+            if (ex != null && ex.series == neighbour.series) themeCount++;
 
         }
 
