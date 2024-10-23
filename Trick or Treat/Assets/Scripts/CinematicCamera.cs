d5df4c3881fb8b023faa8472a7c66e71582f422e
Paigro <paigro9@gmail.com>
2024-10-23 13:01:49 +0200

    Gañan gañanada gañanadanas gamadanadas gañananadas como cojoens se dice.

--- Trick or Treat/Assets/Scripts/CinematicCamera.cs
+++ Trick or Treat/Assets/Scripts/CinematicCamera.cs
@@ -5,15 +5,10 @@ using UnityEngine.Playables;
 
 public class CinematicCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
+        // Usted pudiere ver este fragmento de codigo y pensar que es una ganyanada, y yo le comunico a usted que efectivamente tiene razon es una ganyanada de manual.
         if (GameManager.Instance.getCurrentState() != GameManager.GameStates.NEIGHBOUR)
         {
             Vector3 pos = new Vector3(495, -1077, 1834);
