5fa765b01ff4f739437ce888e4a42b1df10e90cb
Paigro <paigro9@gmail.com>
2024-10-23 13:03:35 +0200

    Olu.

--- Trick or Treat/Assets/Scripts/CinematicCamera.cs
+++ Trick or Treat/Assets/Scripts/CinematicCamera.cs
@@ -11,7 +11,6 @@ public class CinematicCamera : MonoBehaviour
         // Usted pudiere ver este fragmento de codigo y pensar que es una ganyanada, y yo le comunico a usted que efectivamente tiene razon es una ganyanada de manual.
         if (GameManager.Instance.getCurrentState() != GameManager.GameStates.NEIGHBOUR)
         {
-            Vector3 pos = new Vector3(495, -1077, 1834);
             this.GetComponentInParent<PlayableDirector>().time = 0;
         }
     }
